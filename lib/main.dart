bacba56e3d9401493d75602a6a76bed28312c491
Tuan VDIT <tuan.nguyen@vdit.co.uk>
2022-10-14 11:25:58 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'amplifyconfiguration.dart';
@@ -36,10 +37,95 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: SizedBox(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            
+            children: [
+              GestureDetector(
+                onTap: () {
+                  signUpUser();
+                  print(isSignUpComplete);
+                },
+                child: Text("Sign Up"),
+              ),
+              GestureDetector(
+                onTap: () {
+                  confirmUser();
+                },
+                child: Text("Confirm"),
+              ),
+              GestureDetector(
+                onTap: () {
+                  signInUser('test1', '123456');
+                },
+                child: Text("Sign In"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+// ------------------------------------------
+
+  bool isSignUpComplete = false;
+
+  Future<void> signUpUser() async {
+    try {
+      final userAttributes = <CognitoUserAttributeKey, String>{
+        CognitoUserAttributeKey.email: 'tuan.nguyen@vdit.co.uk',
+        CognitoUserAttributeKey.phoneNumber: '+447123456789',
+        // additional attributes as needed
+      };
+      final result = await Amplify.Auth.signUp(
+        username: 'test1',
+        password: '123456',
+        options: CognitoSignUpOptions(userAttributes: userAttributes),
+      );
+      setState(() {
+        isSignUpComplete = result.isSignUpComplete;
+      });
+    } on AuthException catch (e) {
+      safePrint(e.message);
+    }
+  }
+
+  Future<void> confirmUser() async {
+    try {
+      final result = await Amplify.Auth.confirmSignUp(
+          username: 'test1', confirmationCode: '566231');
+
+      setState(() {
+        isSignUpComplete = result.isSignUpComplete;
+      });
+    } on AuthException catch (e) {
+      safePrint(e.message);
+    }
+  }
+
+
+// ------------------------------------------
+
+bool isSignedIn = false;
+
+Future<void> signInUser(String username, String password) async {
+  try {
+    final result = await Amplify.Auth.signIn(
+      username: username,
+      password: password,
+    );
+
+    setState(() {
+      isSignedIn = result.isSignedIn;
+    });
+
+  } on AuthException catch (e) {
+    safePrint(e.message);
+  }
+}
+
 }
